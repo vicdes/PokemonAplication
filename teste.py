ed86d2b796630f7d4c154a7aaab622d8fdc219b6
José Eduardo <joseeduardomj@gmail.com>
2023-05-09 11:50:19 -0300

    dando update no git

--- teste.py
+++ teste.py
@@ -1,7 +1,11 @@
 
 from controladores.controladorPokedex import *
+from controladores.controladorTime import *
 
+'''controlador_pokedex = ControladorPokedex()
 
-controlador_pokedex = ControladorPokedex()
+controlador_pokedex.abre_tela()'''
 
-controlador_pokedex.abre_tela()
+controlador_time = ControladorTime()
+
+controlador_time.abre_tela()
